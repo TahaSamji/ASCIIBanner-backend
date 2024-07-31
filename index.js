9606a629e540e2051b5542e5520bbfbb69a114eb
Right 97 <Right.97@rs.net>
2024-07-31 11:49:04 +0500

    vercel commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const port = 8000
 
 const corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }

c191dc1ac3a81730992e967d57161a285d10fc06
Right 97 <Right.97@rs.net>
2024-08-01 16:12:23 +0500

    vercel commit

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const cors = require("cors");
 const port = 8000
 
 const corsOptions = {
-  origin: "https://ascii-banner.vercel.app",
+  // origin: "https://ascii-banner.vercel.app",
+  origin: "http://localhost:3000",
+  
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }
+let items = [
+  "Alpha", "1Row", "3x5", "AMC 3 Liv1", "AMC Tubes", "Wow", "Trek", "Ticks", 
+  "Ticks Slant", "Test1", "Tengwar", "Stellar", "Star Strips", "Small Tengwar", 
+  "Small Poison", "Slide", "S Blood", "Runyc", "Runic", "Rotated", "Rot13", 
+  "Reverse", "Relief", "Relief2", "Poison", "Peaks", "Peaks Slant", 
+  "Patorjk-HeX", "OS2", "Octal", "Nipples", "Mshebrew210", "Moscow", 
+  "Morse2", "Mike", "Merlin2", "Marquee", "Lockergnome", "Linux", "Lean", 
+  "LCD", "Konto", "Konto Slant", "Knob", "Katakana", "Jerusalem", "Hieroglyphs", 
+  "Hex", "Gradient", "Goofy", "Flipped", "Filter", "Efti Wall", "Efti Piti", 
+  "Efti Chess", "Doh", "Def Leppard", "Decimal", "DANC4", "Cola", "Binary", 
+  "Bear", "B1FF", "AMC Untitled", "AMC Slash", "AMC Razor2", "AMC Neko","Benjamin","Barbwire","Basic","Block","Braced","Bright","Caligraphy"
+  ,"Caligraphy2","Computer","Contrast","Cosmike","Fraktur","JS Cursive","Morse","Rammstein","Tsalagi","USA Flag","Alphabet"
+];
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -56,7 +71,8 @@ app.get('/', (req, res) => {
         console.log("something went wrong...");
         console.dir(err);
         return;
-      }
+      } 
+      fonts = fonts.filter(font => !items.includes(font));
     return res.json({data:fonts})
     });
   } catch (error) {
